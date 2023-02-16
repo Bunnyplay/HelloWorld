dd373aaec8ca9895d62758978a5fa91ace9623a4
Bunnyplay <89224505+Bunnyplay@users.noreply.github.com>
2023-02-16 14:08:10 -0500

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 2, y =3, z = 5, a = 4, b = 6, c = 7; 
+            if (x == 2)
+                if(y == 3)
+                    if(z == 5)
+                        if(a == 4)
+                            if(b == 6)
+                                if(c == 7)
+                                    Console.WriteLine("Hello World!");
         }
     }
 }
